4c6df22fe7c7f44bb5e3ad191aee318bb99d0ee9
Glory Ngum Foncha <gloriane7767@gmail.com>
2025-12-16 10:04:25 +0100

    add bank account class

---
+++ src/main/java/com/gloriane/BankAccount.java
@@ -0,0 +1,50 @@
+package com.gloriane;
+
+public class BankAccount {
+    private String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountHolder, double initialBalance) {
+        this.accountHolder= accountHolder;
+        this.balance = initialBalance;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String setAccountHolder(String name) {
+        this.accountHolder = name;
+        return accountHolder;
+    }
+
+    public double setBalance(double amount) {
+        this.balance = amount;
+        return balance;
+    }
+
+    public double showBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            throw new IllegalArgumentException("Deposit amount must be positive");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        } else {
+            throw new IllegalArgumentException("Invalid withdrawal amount");
+        }
+    }
+}
+
