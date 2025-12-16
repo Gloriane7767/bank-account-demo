99f7414f7d3e62bbfcdd2bb9c615c6b1a991849c
Glory Ngum Foncha <gloriane7767@gmail.com>
2025-12-16 11:31:11 +0100

    add  UML diagram and customer class

---
+++ src/main/java/com/gloriane/Customer.java
@@ -0,0 +1,40 @@
+package com.gloriane;
+
+import java.util.UUID;
+
+public class Customer {
+    private  int id;
+    private String name;
+    private String email;
+
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        if(name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        if(email == null || !email.contains("@")) {
+            throw new IllegalArgumentException("Invalid email address");
+        }
+        this.email = email;
+    }
+
+    public int setId() {
+        return UUID.randomUUID().toString().substring(0,8).hashCode();
+    }
+}
